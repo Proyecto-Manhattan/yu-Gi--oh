3068766dd63afc574f5fc3f39fc1a710f7b08cf6
VICTOR DE PAULA OSORNIO MENDEZ <vosorniom@itam>
2016-12-07 12:27:09 -0600

    es hora de de de de el duelo

---
+++ YgoPro/script/c29455728.lua
@@ -0,0 +1,42 @@
+--ツイン・フォトン・リザード
+function c29455728.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x55),2,true)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(29455728,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(c29455728.cost)
+	e1:SetTarget(c29455728.target)
+	e1:SetOperation(c29455728.operation)
+	c:RegisterEffect(e1)
+end
+function c29455728.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c29455728.mgfilter(c,e,tp,fusc)
+	return not c:IsControler(tp) or not c:IsLocation(LOCATION_GRAVE)
+		or bit.band(c:GetReason(),0x40008)~=0x40008 or c:GetReasonCard()~=fusc
+		or not c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c29455728.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetHandler():GetMaterial()
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and g:GetCount()>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)+1>=g:GetCount()
+		and bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+		and not g:IsExists(c29455728.mgfilter,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
+end
+function c29455728.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	local g=e:GetHandler():GetMaterial()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>=g:GetCount()
+		and not g:IsExists(c29455728.mgfilter,1,nil,e,tp,e:GetHandler())
+		and not g:IsExists(Card.IsHasEffect,1,nil,EFFECT_NECRO_VALLEY) then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
